bdb5e2a061e8712c22b9b744e25522841ba9e5c5
kritishrivastava257-del <kritishrivastava257@gmail.com>
2026-02-14 10:54:03 +0530

    project setup done

---
+++ src/Pages/Favourite.jsx
@@ -0,0 +1,8 @@
+function Favourite() {
+  return (
+    <div>
+      <h1>Favourite</h1>
+    </div>
+  );
+}
+export default Favourite;
---
+++ src/Pages/MovieDetails.jsx
@@ -0,0 +1,8 @@
+function MovieDetails() {
+  return (
+    <div>
+      <h1>Movie Details</h1>
+    </div>
+  );
+}
+export default MovieDetails;
